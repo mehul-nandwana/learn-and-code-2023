27e59b0ecea1a470fa3b7dfef34d161d0fa0cec0
Mehul Nandwana <mnandwana@pharos.com>
2024-01-17 18:31:06 +0530

    Updated code

---
+++ CarbonFootprint/CarbonFootprint/Constants.cs
@@ -0,0 +1,10 @@
+﻿namespace Solution
+{
+    public class Constants
+    {
+        public const double intensityOfCarbon = 0.5;
+        public const double gmailCarbonIntensity = 0.3;
+        public const double yahooCarbonIntensity = 0.5;
+        public const double outlookCarbonIntensity = 0.7;
+    }
+}
